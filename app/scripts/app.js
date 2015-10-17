cbe23692543badbe5660594025aebfe61aa65bb4
Diana.Alexandru <diana.alexandru@teamnet.ro>
2015-10-17 22:20:00 +0300

    added nav and link2 page

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -8,21 +8,55 @@ angular
     'ngResource',
     'ngRoute',
     'ngSanitize',
-    'ngTouch'
+    'ngTouch',
+    'ui.router'
   ])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl',
-        controllerAs: 'main'
-      })
-      .when('/affiliates/tools/link2', {
+  .config(function ($stateProvider, $urlRouterProvider, $httpProvider) {
+    //enable CSRF
+    $httpProvider.defaults.xsrfCookieName= 'CSRF-TOKEN';
+    $httpProvider.defaults.xsrfHeaderName= 'X-CSRF-TOKEN';
+
+    $urlRouterProvider.otherwise('/affiliates/dashboard');
+    $stateProvider
+    .state('dashboard', {
+        url: "/affiliates/dashboard",
+        templateUrl: 'views/main.html'
+    })
+    .state('affiliate', {
+        url: "/affiliates/affiliate",
+        templateUrl: 'views/main.html'
+    })
+    .state('commissions', {
+        url: "/affiliates/commissions",
+        templateUrl: 'views/main.html'
+    })
+    .state('statistics', {
+        url: "/affiliates/statistics",
+        templateUrl: 'views/main.html'
+    })
+    .state('link2', {
+        url: "/affiliates/tools/link2",
         templateUrl: 'views/link2.html',
-        controller: 'Link2Ctrl',
-        controllerAs: 'link2'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
+        controller: 'Link2Ctrl'
+    })
+    .state('banners', {
+        url: "/affiliates/tools/banners",
+        templateUrl: 'views/main.html'
+    })
+    .state('product', {
+        url: "/affiliates/tools/product",
+        templateUrl: 'views/main.html'
+    })
+    .state('feeds', {
+        url: "/affiliates/tools/feeds",
+        templateUrl: 'views/main.html'
+    })
+    .state('messages', {
+        url: "/affiliates/messages",
+        templateUrl: 'views/main.html'
+    })
+    .state('settings', {
+        url: "/affiliates/settings",
+        templateUrl: 'views/main.html'
+    });
   });
--- app/scripts/controllers/main.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-
-angular.module('link2App')
-  .controller('MainCtrl', function () {
-  });
