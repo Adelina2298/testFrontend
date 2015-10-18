86d351162a40670f44eb29c63f70e80ad1bc0582
Diana.Alexandru <diana.alexandru@teamnet.ro>
2015-10-18 14:19:40 +0300

    added include/exclude button and convert button

--- app/scripts/controllers/link2.js
+++ app/scripts/controllers/link2.js
@@ -6,6 +6,7 @@ angular.module('link2App')
   		$scope.allprograms = [];
 	    $scope.programs = [];
 	    $scope.filterSelected = true;
+	    $scope.include = true;
 
 	    // Search for programs
   		$scope.querySearch = function(query) {
@@ -20,7 +21,7 @@ angular.module('link2App')
 	      return function filterFn(programs) {
 	        return (programs.name.toLowerCase().indexOf(lowercaseQuery) != -1);
 	      };
-	    }
+	    };
 
   		$scope.loadPrograms = function() {
 			Programs.getAll()
@@ -30,7 +31,25 @@ angular.module('link2App')
                 .error(function(){
                     alert('There has been an error. Please try again later!');
                 });
-	    }
+	    };
 
 	    $scope.loadPrograms();
+
+	    $scope.addProgram = function(program) {
+	    	$scope.programs.push(program);
+	    }
+
+	    $scope.convert = function() {
+	    	if ($scope.include) {
+	    		for (var i = 0; i < $scope.programs.length; i++) {
+	    			console.log($scope.programs[i].unique_code);
+	    		}
+	    	} else {
+	    		for (var i = 0; i < $scope.allprograms.length; i++) {
+	    			console.log("nustcesafac");
+	    		}
+	    	}
+	    }
+
+	    
   }));
